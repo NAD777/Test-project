d7c9352c48255c274635e2d571434f1de6d5a7d8
NAD777 <nehaev07.07.2003@gmail.com>
2019-09-03 19:16:36 +0100

    mem and time support

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import time
 
 class Test:
     def __init__(self):
-        pass
+        self.tl_time = 1  # time in sec
+        self.ml_memory = 16 * 1024  # in kB 
 
     def compile(self, name_file, out_name="a.out"):
         sp.call(["g++", "-std=c++2a", name_file, "-o", out_name])
@@ -28,25 +29,45 @@ class Test:
 
     def mem(self, pid): # returns mem in kB
         time_start = time.time()
-        col_mem = sp.check_output([f"cat /proc/{pid}/status | grep -i VMSIZE"], shell=True).rstrip()
-        return col_mem.decode()[11:-3]
+        try:
+            col_mem = sp.check_output([f"cat /proc/{pid}/status | grep -i VMSIZE"], shell=True).rstrip()
+            return int(col_mem.decode()[11:-3])
+        except BaseException:
+            return 0  # todo: refactor it
 
-    def for_test(self, test_file, file_name='a.out'): #WORKS!!! ALEXEY LOOK HERE!!!!!
+    def for_test(self, test_file, file_name='a.out'):
             with open(test_file) as inp:
                 tl = False
-                proc = sp.Popen([f'./{file_name}'], shell=True, stdin=inp, stdout=sp.PIPE, stderr=sp.PIPE)
-                time.sleep(1)
-                if proc.poll() is None:
-                    tl = True
-                    proc.kill()
+                ml = False
+                proc = sp.Popen([f'exec ./{file_name}'], shell=True, stdin=inp, stdout=sp.PIPE, stderr=sp.PIPE)
+                time_start = time.time()
+                print(proc.pid)
+                while True:
+                    if proc.poll() is None and time.time() - time_start >= self.tl_time:
+                        tl = True
+                        proc.kill()
+                        break
+                    
+                    elif self.mem(proc.pid) >= self.ml_memory:
+                        ml = True
+                        proc.kill()
+                        break
+
+                    elif proc.poll() is not None:
+                        break
+
+                if tl:
+                    return "TL"
+                if ml:
+                    return "ML"
                 output, err = proc.communicate()
                 ret = proc.returncode
                 return output, ret, err, proc.pid, tl
 
 
 test = Test()
-print(test.mem("23229"))
-# print(test.for_test("tests/0", 'a.out'))
+# print(test.mem("23229"))
+print(test.for_test("0", 'a.out'))
 # print(test.test("tests"))
 # print(test.run_test("tests/0", "tests/0.a"))
 # test.compile("main.cpp")
