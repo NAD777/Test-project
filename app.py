a2b75894a1ec82176ee3978e57935b8c585c39cc
NAD777 <nehaev07.07.2003@gmail.com>
2019-09-07 15:55:49 +0300

    Database insted of files

--- app.py
+++ app.py
@@ -1,16 +1,63 @@
 from flask import Flask, render_template, redirect
-from read import read, get_json
-from json import loads
-import os
+from read import read, get_json, string_to_dict
+from flask_sqlalchemy import SQLAlchemy
 
 
-
+COL_PROBLEMS_ONE_PAGE = 10
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Problem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=False, nullable=False)
+    memory = db.Column(db.Integer, unique=False, nullable=False)
+    time = db.Column(db.Integer, unique=False, nullable=False)
+    difficulty = db.Column(db.Integer, unique=False, nullable=False)
+    condition = db.Column(db.Text(), unique=False, nullable=False)
+    inp = db.Column(db.Text(), unique=False, nullable=False)
+    output = db.Column(db.Text(), unique=False, nullable=False)
+    examples = db.Column(db.Text(), unique=False, nullable=False)  # rasparse
+   
+    def __repr__(self):
+        return '<Problem {} {}>'.format(self.id, self.name)
+
+
+
+db.create_all()
+
+
+@app.route("/test-problem/")
+def test_problem():
+    print(Problem.query.all())
+    return f"{Problem.query.all()}"
+
+@app.route("/test-add/")
+def test_add():
+    task = Problem(name='A + B',
+                    memory=16,
+                    time=1,
+                    difficulty=1,
+                    condition='А + B',
+                    inp="Два числа",
+                    output="Сумма двух чисел",
+                    examples="1 2~3|3 4~7"
+                    ) 
+    db.session.add(task)
+    db.session.commit()
+    return redirect("/")
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 @app.route('/status/')
 def status():
@@ -18,11 +65,52 @@ def status():
     a = list(map(lambda x: x.split('~'), a))
     return render_template("status.html", content=a)
 
+
+@app.route("/problemset/list/<num>/")
+def problemset(num): 
+    n = int(num)
+    print(Problem.query.all())  
+    arr = Problem.query.all()[(n - 1) * COL_PROBLEMS_ONE_PAGE:n * COL_PROBLEMS_ONE_PAGE]
+    content = [(el.id, el.name, el.difficulty) for el in arr]
+    return render_template("problemset.html", content_table=content)
+
+
 @app.route('/problemset/<num>/')
-def problemset(num):
-    content = get_json(f"problems/{num}/cfg.json")
+def problemset_num(num):
+    n = int(num)
+    problem = Problem.query.filter_by(id=n).first()
+    # content = get_json(f"problems/{num}/cfg.json")
     # print(type(content))
+    content = {
+        "name": problem.name,
+        "mem": problem.memory,
+        "time": problem.time,
+        "difficulty": problem.difficulty,
+        "condition": problem.condition,
+        "inp": problem.inp,
+        "output": problem.output,
+        "examples":  string_to_dict(problem.examples)
+    }
     return render_template("problem.html", data=content)
+
+
+@app.route('/add/')
+def add():
+    return render_template("add.html")
+
+
+@app.route('/add/<string>')
+def add_send(string):
+    # return string
+    id, name, memory, time, difficulty, \
+            condition, inp, output, examples = map(str, string.split("~"))
+
+    prm = Problem(name=name, memory=int(memory), time=int(time), 
+            difficulty=int(difficulty), condition=condition, inp=inp, 
+             output=output, examples=examples)
+    db.session.add(prm)
+    db.session.commit()
+    return redirect('/add/')
 
 
 if __name__ == '__main__':
--- read.py
+++ read.py
@@ -1,15 +1,24 @@
 import json
-import os
+
 
 def read(name):
     with open(name, 'r') as file:
         return [line.rstrip() for line in file]
+
 
 def get_json(path):
     with open(path, "r") as file:
         d = json.load(file)
         return d
 
+
+def string_to_dict(string):  # "1 2~3|3 4~7" -> dict("1 2": "3", "3 4": "7")
+    d = dict()
+    for text_with_ans in string.split('|'):
+        test, ans = text_with_ans.split('~')
+        d[test] = ans
+    return d
+
+
 if __name__ == "__main__":
-    print(os.path.dirname(os.path.realpath("problems/1/cfg.json")))
-    print(get_json("problems/1/cfg.json"))
+    print(repr(string_to_dict("1 2~3|3 4~7")))
